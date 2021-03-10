6cd96f7ff69f8f88a7c4e51bddeb4146f5c7e86d
nchintada <nchintada@wpi.edu>
2021-03-10 17:22:37 -0500

    Remnant from testing

--- Bomberman/groupNN/scenario1/variant4.py
+++ Bomberman/groupNN/scenario1/variant4.py
@@ -21,11 +21,22 @@ g.add_monster(SelfPreservingMonster("aggressive", # name
                                     2             # detection range
 ))
 
+# g.add_monster(SelfPreservingMonster("aggressive", # name
+#                                     "A",          # avatar
+#                                     3, 3,        # position
+#                                     2             # detection range
+# ))
+
 # TODO Add your character
 g.add_character(TestCharacter("me", # name
                               "C",  # avatar
                               0, 0  # position
 ))
+
+# g.add_character(TestCharacter("me", # name
+#                               "C",  # avatar
+#                               7, 1  # position
+# ))
 
 # Run!
 g.go()
